7edfbd7090f2077c51602e31e164c42e712b19ca
Taimoor Sohail <taimoorsohail@gmail.com>
2025-03-29 13:59:18 +1100

    Added MWEs that illustrate the issue with the condition constraint

--- mwe_condition_working.jl
+++ mwe_condition_working.jl
@@ -1,5 +1,6 @@
 using Oceananigans
 using Oceananigans.Units
+using ClimaOcean
 includet("BasinMask.jl")
 using .BasinMask
 
@@ -10,14 +11,30 @@ Nz = Integer(100/4)
 arch = CPU()
 z_faces = (-4000, 0)
 
-grid = TripolarGrid(arch;
+underlying_grid = TripolarGrid(arch;
                     size = (Nx, Ny, Nz),
                     z = z_faces,
                     halo = (5, 5, 4),
                     first_pole_longitude = 70,
                     north_poles_latitude = 55)
 
-free_surface = SplitExplicitFreeSurface(grid; substeps=10)
+@info "Defining bottom bathymetry"
+
+@time bottom_height = regrid_bathymetry(underlying_grid;
+                                    minimum_depth = 10,
+                                    interpolation_passes = 75, # 75 interpolation passes smooth the bathymetry near Florida so that the Gulf Stream is able to flow
+                                    major_basins = 2)
+
+# For this bathymetry at this horizontal resolution we need to manually open the Gibraltar strait.
+# view(bottom_height, 102:103, 124, 1) .= -400
+
+@info "Defining grid"
+
+@time grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(bottom_height); active_cells_map=true)
+                    
+@info grid           
+
+free_surface = SplitExplicitFreeSurface(grid; substeps=30)
 model = HydrostaticFreeSurfaceModel(; grid, free_surface)
 
 
@@ -31,9 +48,9 @@ function integrate_tuple(outputs; volmask, dims, condition, suffix::AbstractStri
     end
     @info "I am doing dV now"
     dV_int = NamedTuple{(Symbol(:dV, suffix),)}((Integral(volmask; dims, condition),))
-    @info "I am merging now"
-    int_outputs = merge(int_model_outputs, dV_int)
-    return int_outputs
+    # @info "I am merging now"
+    # int_outputs = merge(int_model_outputs, dV_int)
+    return nothing #int_model_outputs#int_outputs
 end
 
 c = CenterField(grid)
@@ -45,7 +62,7 @@ Atlantic_mask = repeat(basin_mask(grid, "atlantic", c, arch), 1, 1, Nz)
 IPac_mask = repeat(basin_mask(grid, "indo-pacific", c, arch), 1, 1, Nz)
 glob_mask = Atlantic_mask .|| IPac_mask
 
-@info size(Atlantic_mask), size(IPac_mask), size(glob_mask))
+@info size(Atlantic_mask), size(IPac_mask), size(glob_mask)
 
 tracers = model.tracers
 velocities = model.velocities
@@ -56,4 +73,4 @@ outputs = merge(tracers, velocities)
 @time Atlantic_zonal_int_outputs = integrate_tuple(outputs; volmask, dims = (1), condition = Atlantic_mask, suffix = "_atlantic")
 @time IPac_zonal_int_outputs = integrate_tuple(outputs; volmask, dims = (1), condition = IPac_mask, suffix = "_pacific")
 
-@time zonal_int_outputs = merge(global_zonal_int_outputs, Atlantic_zonal_int_outputs, IPac_zonal_int_outputs)
+# @time zonal_int_outputs = merge(global_zonal_int_outputs, Atlantic_zonal_int_outputs, IPac_zonal_int_outputs)
