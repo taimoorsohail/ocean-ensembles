b010d8333d6822eee8d9fca6ab81c267fd3f7d28
Taimoor Sohail <taimoorsohail@gmail.com>
2025-04-07 22:08:31 +1000

    Changes to the Diagnostics function; simplified outputs

--- src/Diagnostics.jl
+++ src/Diagnostics.jl
@@ -3,14 +3,14 @@ module Diagnostics
     using Oceananigans
     using Oceananigans.Fields: location
 
-    export integrate_tracer, integrate_transport
+    export ocean_tracer_content, volume_transport
 
-    function integrate_tracer(outputs; metric, dims, condition, suffix::AbstractString)
+    function ocean_tracer_content(outputs; operator, dims, condition, suffix::AbstractString)
         names = []
         ∫outputs = []
         for key in keys(outputs)
             f = outputs[key]
-            ∫f = Integral(f * metric; dims, condition)
+            ∫f = Integral(f * operator; dims, condition)
             push!(∫outputs, ∫f)
             push!(names, Symbol(key, suffix))
         end
@@ -18,31 +18,34 @@ module Diagnostics
         onefield = CenterField(first(outputs).grid)
         set!(onefield, 1)
 
-        ∫dV = Integral(onefield * metric; dims, condition)
+        ∫dV = Integral(onefield * operator; dims, condition)
         push!(∫outputs, ∫dV)
         push!(names, Symbol(:dV, suffix))
 
         return NamedTuple{Tuple(names)}(∫outputs)
     end
 
-    function integrate_transport(outputs; metrics, dims, condition, suffix::AbstractString)
+    function volume_transport(outputs; operators, dims, condition, suffix::AbstractString)
         names = []
         ∫outputs = []
-        for (i, key) in enumerate(keys(outputs))
-            f = outputs[key]
-            ∫f = sum(f * metrics[i]; dims, condition)
-            push!(∫outputs, ∫f)
-            push!(names, Symbol(key, suffix))
+        if size(keys(outputs)) == size(operators)
+            for (i, key) in enumerate(keys(outputs))
+                f = outputs[key]
+                ∫f = sum(f * operators[i]; dims, condition)
+                push!(∫outputs, ∫f)
+                push!(names, Symbol(key, suffix))
 
-            LX, LY, LZ = location(f)
-            onefield = Field{LX, LY, LZ}(f.grid)
-            set!(onefield, 1)
+                LX, LY, LZ = location(f)
+                onefield = Field{LX, LY, LZ}(f.grid)
+                set!(onefield, 1)
 
-            ∫dV = sum(onefield * metrics[i]; dims, condition)
-            push!(∫outputs, ∫dV)
-            push!(names, Symbol(:dV, "_$(key)", suffix))
+                ∫dV = sum(onefield * operators[i]; dims, condition)
+                push!(∫outputs, ∫dV)
+                push!(names, Symbol(:dV, "_$(key)", suffix))
+            end
+        else
+            throw(ArgumentError("The number of operators must be equal to the number of tracers"))
         end
-
         return NamedTuple{Tuple(names)}(∫outputs)
     end
 end
