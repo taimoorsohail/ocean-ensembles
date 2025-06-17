96f5a308198f633a61a19a3620a316cfe3906fac
Taimoor Sohail <taimoorsohail@gmail.com>
2025-06-17 22:08:21 +1000

    Working towards a working restart system on MPI

--- test/test_restart.jl
+++ test/test_restart.jl
@@ -20,7 +20,7 @@ using Glob
 data_path = expanduser("/g/data/v46/txs156/ocean-ensembles/data/")
 output_path = expanduser("/g/data/v46/txs156/ocean-ensembles/outputs/")
 figdir = expanduser("/g/data/v46/txs156/ocean-ensembles/figures/")
-
+target_time = 365days
 ## Argument is provided by the submission script!
 
 arch = Distributed(GPU(); partition = Partition(y = DistributedComputations.Equal()), synchronized_communication=true)
@@ -28,27 +28,31 @@ total_ranks = MPI.Comm_size(MPI.COMM_WORLD)
 
 @info "Using architecture: " * string(arch)
 
-# clockfiles = glob("clock_distributedGPU*", output_path)
+clockfiles = glob("clock_distributedGPU_iteration*", output_path)
 
-# iteration = 0
-# time = 0.0
-# if !isempty(clockfiles)
-#     # Extract the numeric suffix from each filename
-#     numbers = map(f -> parse(Int, match(r"clock_distributedGPU_(\d+)", basename(f)).captures[1]), clockfiles)
+iteration = 0
+time = 0.0
+if !isempty(clockfiles)
+    # Extract the numeric suffix from each filename
+    numbers = map(f -> parse(Int, match(r"clock_distributedGPU_iteration(\d+)", basename(f)).captures[1]), clockfiles)
 
-#     # Get the file with the maximum number
-#     max_index = argmax(numbers)
-#     maxiter = clockfiles[max_index]
+    # Get the file with the maximum number
+    max_index = argmax(numbers)
+    maxiter = clockfiles[max_index]
+    @show maxiter
+    clock_vars = jldopen(maxiter)
 
-#     clock_vars = jldopen(maxiter)
+    iteration = clock_vars["clock"].iteration
+    time = clock_vars["clock"].time
+    @info "Moving simulation to " * string(iteration) * " iterations"
+    @info "Moving simulation to " * string(prettytime(time))
 
-#     iteration = clock_vars["iteration"]
-#     time = clock_vars["time"]
-#     @info "Moving simulation to " * string(iteration) * " iterations"
-#     @info "Moving simulation to " * string(time) * " time"
+    close(clock_vars)
+end
 
-# end
-
+if time == target_time
+    error("Terminating simulation at target time.")
+end
 # ### Grid and Bathymetry
 @info "Defining grid"
 
@@ -131,6 +135,13 @@ atmosphere = JRA55PrescribedAtmosphere(arch; backend=JRA55NetCDFBackend(20))
 coupled_model = OceanSeaIceModel(ocean; atmosphere, radiation)
 simulation = Simulation(coupled_model; Δt=5minutes, stop_time=10days)
 
+simulation.model.ocean.model.clock.iteration = iteration
+simulation.model.ocean.model.clock.time = time
+simulation.model.atmosphere.clock.iteration = iteration
+simulation.model.atmosphere.clock.time = time
+simulation.model.clock.iteration = iteration
+simulation.model.clock.time = time
+
 # ### A progress messenger
 #
 # We write a function that prints out a helpful progress message while the simulation runs.
@@ -154,7 +165,7 @@ function progress(sim)
         
     step_time = 1e-9 * (time_ns() - wall_time[])
 
-    msg1 = @sprintf("time: %s, iteration: %d, Δt: %s, ", prettytime(sim), iteration(sim), prettytime(sim.Δt))
+    msg1 = @sprintf("time: %s, iteration: %d, Δt: %s, ", prettytime(sim), Oceananigans.iteration(sim), prettytime(sim.Δt))
     msg2 = @sprintf("max|u|: (%.2e, %.2e, %.2e) m s⁻¹, ", umax...)
     msg3 = @sprintf("extrema(T): (%.2f, %.2f) ᵒC, ", Trange...)
     msg4 = @sprintf("extrema(S): (%.2f, %.2f) g/kg, ", Srange...)
@@ -178,15 +189,13 @@ velocities = ocean.model.velocities
 
 outputs = merge(tracers, velocities)
 
-@info "Saving restart"
-
 output_intervals = AveragedTimeInterval(5days)
 checkpoint_intervals = TimeInterval(5days)
 
 simulation.output_writers[:surface] = JLD2Writer(ocean.model, outputs;
                                                  dir = output_path,
                                                  schedule = output_intervals,
-                                                 filename = "global_surface_fields_distributedGPU",
+                                                 filename = "global_surface_fields_distributedGPU_iteration" * string(simulation.model.clock.iteration),
                                                  indices = (:, :, grid.Nz),
                                                  with_halos = false,
                                                  overwrite_existing = true,
@@ -197,94 +206,113 @@ fluxes = coupled_model.interfaces.atmosphere_ocean_interface.fluxes
 simulation.output_writers[:fluxes] = JLD2Writer(ocean.model, fluxes;
                                                 dir = output_path,
                                                 schedule = output_intervals,
-                                                filename = "fluxes_distributedGPU",
+                                                filename = "fluxes_distributedGPU_iteration" * string(simulation.model.clock.iteration),
                                                 overwrite_existing = true)
+@info "Saving restart"
+restart_file = "test_checkpoint"
+# simulation.output_writers[:full_field] = JLD2Writer(ocean.model, outputs;
+#                                                 dir = output_path,
+#                                                 schedule = TimeInterval(5days),
+#                                                 filename = restart_file * "_" * string(simulation.model.clock.iteration),
+#                                                 with_halos = false,
+#                                                 overwrite_existing = true,
+#                                                 array_type = Array{Float32})
 
-simulation.output_writers[:checkpoint] = Checkpointer(ocean.model;
-                                        dir = output_path,
-                                        schedule = checkpoint_intervals,
-                                        prefix = "test_checkpoint",
-                                        properties = [:grid, :particles, :clock],
-                                        overwrite_existing = true)
-# function save_clock(sim)
-#     if MPI.Comm_rank(MPI.COMM_WORLD) == 0
-#         jldsave(output_path * "clock_distributedGPU_" * string(sim.model.clock.iteration) * ".jld2",
-#                 sim.model.clock)
-#     end
-# end
+#                                                 @info "Saving restart"
+
+# simulation.output_writers[:checkpoint] = Checkpointer(ocean.model;
+#                                         dir = output_path,
+#                                         schedule = checkpoint_intervals,
+#                                         prefix = "test_checkpoint",
+#                                         properties = [],
+#                                         overwrite_existing = true)
+function save_clock(sim)
+    if arch.local_rank == 0
+        jldsave(output_path * "clock_distributedGPU_iteration" * string(sim.model.clock.iteration) * ".jld2",
+        clock=sim.model.clock)
+    end
+    jldsave(output_path * "checkpoint_iteration" * string(sim.model.clock.iteration) * "_rank" * string(arch.local_rank) * ".jld2";
+    ocean.model.tracers, ocean.model.velocities)
+end
                                                                                                         
-# add_callback!(simulation, save_clock, TimeInterval(5days))
+add_callback!(simulation, save_clock, TimeInterval(5days))
 
-restartfiles = glob("test_checkpoint*", output_path)
+restartfiles = glob("checkpoint_iteration*", output_path)
 
-if !isempty(restartfiles)
     # Extract the numeric suffix from each filename
-    numbers = map(f -> parse(Int, match(r"test_checkpoint_(\d+)", basename(f)).captures[1]), restartfiles)
-
+numbers = map(f -> parse(Int, match(r"checkpoint_iteration(\d+)", basename(f)).captures[1]), restartfiles)
+if !isempty(numbers) && maximum(numbers) != 0
     # Get the file with the maximum number
-    max_index = argmax(numbers)
-    maxiter = restartfiles[max_index]
+    # max_index = argmax(numbers)
+    # maxiter = restartfiles[max_index]
 
-    # @info "Loading with restart file from iteration" * string(max(numbers))
+    @info "Loading with restart file from iteration" * string(maximum(numbers))
+    @info "Local rank ", arch.local_rank
+    @info output_path * "checkpoint_iteration" * string(maximum(numbers)) * "_rank" * string(arch.local_rank) * ".jld2"
 
-    # T_field = FieldTimeSeries(maxiter, "T")
-    # S_field = FieldTimeSeries(maxiter, "S")
-    # u_field = FieldTimeSeries(maxiter, "u")
-    # v_field = FieldTimeSeries(maxiter, "v")
-    # w_field = FieldTimeSeries(maxiter, "w")
-    # e_field = FieldTimeSeries(maxiter, "e")
+    fields = jldopen(output_path * "checkpoint_iteration" * string(maximum(numbers)) * "_rank" * string(arch.local_rank) * ".jld2")
 
-    # set!(ocean.model, 
-    # T=T_field,
-    # S=S_field,
-    # u=u_field,
-    # v=v_field,
-    # w=w_field,
-    # e=e_field)
+    T_field = fields["tracers"].T 
+    S_field = fields["tracers"].S
+    e_field = fields["tracers"].e
+    u_field = fields["velocities"].u
+    v_field = fields["velocities"].v
+    w_field = fields["velocities"].w
 
-    # Trange = (maximum((T)), minimum((T)))
-    # Srange = (maximum((S)), minimum((S)))
-    # erange = (maximum((e)), minimum((e)))
+    close(fields)
 
-    # umax = (maximum(abs, (u)),
-    #         maximum(abs, (v)),
-    #         maximum(abs, (w)))
+    set!(ocean.model, 
+    T=T_field,
+    S=S_field,
+    u=u_field,
+    v=v_field,
+    w=w_field,
+    e=e_field)
 
-    # @test abs(Trange[1])>0
-    # @test abs(Trange[2])>0
-    # @test abs(Srange[1])>0
-    # @test abs(Srange[2])>0
-    # @test abs(erange[1])>0
-    # @test abs(erange[2])>0
-    # @test abs(umax[1])>0
-    # @test abs(umax[2])>0
-    # @test abs(umax[3])>0
+    Trange = (maximum((T)), minimum((T)))
+    Srange = (maximum((S)), minimum((S)))
+    erange = (maximum((e)), minimum((e)))
 
-    @info "Running simulation"
+    umax = (maximum(abs, (u)),
+            maximum(abs, (v)),
+            maximum(abs, (w)))
 
-    run!(simulation, pickup = maxiter)
+    @show Trange, Srange, erange, umax
+    
+    @test abs(Trange[1])>0
+    @test abs(Trange[2])>0
+    @test abs(Srange[1])>0
+    @test abs(Srange[2])>0
+    @test abs(erange[1])>0
+    @test abs(erange[2])>0
+    @test abs(umax[1])>0
+    @test abs(umax[2])>0
+    @test abs(umax[3])>0
 
-    # combine_outputs(total_ranks, output_path * "restart_distributedGPU_" * string(simulation.model.clock.iteration),
-    #  output_path * "restart_distributedGPU_" * string(simulation.model.clock.iteration); remove_split_files = true)
-    # combine_outputs(total_ranks, output_path * "clock_distributedGPU_" * string(simulation.model.clock.iteration),
-    #  output_path * "clock_distributedGPU_" * string(simulation.model.clock.iteration); remove_split_files = true)
-#     combine_outputs(total_ranks, output_path * "fluxes_distributedGPU_" * string(simulation.model.clock.iteration),
-#     output_path * "fluxes_distributedGPU_" * string(simulation.model.clock.iteration); remove_split_files = true)
-#    combine_outputs(total_ranks, output_path * "global_surface_fields_distributedGPU_" * string(simulation.model.clock.iteration),
-#     output_path * "global_surface_fields_distributedGPU_" * string(simulation.model.clock.iteration); remove_split_files = true)
+    # @info "Running simulation"
+
+    # run!(simulation, pickup = maxiter)
+
 
 
     #  @test isfile(output_path * "restart_distributedGPU_" * string(simulation.model.clock.iteration) * ".jld2")
     #  @test isfile(output_path * "fluxes_distributedGPU_" * string(simulation.model.clock.iteration) * ".jld2")
     #  @test isfile(output_path * "global_surface_fields_distributedGPU_" * string(simulation.model.clock.iteration) * ".jld2")
     #  @test isfile(output_path * "clock_distributedGPU_" * string(simulation.model.clock.iteration) * ".jld2")
+    @info "Restart found at " * string(prettytime(time))
 
+    # run!(simulation)
+
+    simulation.Δt = 5minutes 
+    simulation.stop_time = target_time # 1 year 
+
+    run!(simulation)
 else
     @info "Running simulation"
 
     run!(simulation)
 
-    simulation.Δt = 10minutes 
+    simulation.Δt = 5minutes 
     simulation.stop_time = 365days # 1 year 
 
     run!(simulation)
