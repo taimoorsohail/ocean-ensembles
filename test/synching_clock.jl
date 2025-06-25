8d20fa9e92b378982ae9338034ce15d96cdf03ce
Taimoor Sohail <taimoorsohail@gmail.com>
2025-06-25 13:10:28 +1000

    Making MWE for ClimaOcean issuecd

--- test/synching_clock.jl
+++ test/synching_clock.jl
@@ -2,70 +2,79 @@ using ClimaOcean
 using Oceananigans
 using Oceananigans.Units
 using Oceananigans.DistributedComputations
+using Printf
 
 arch = GPU()
-
-# using MPI
-# using CUDA
-
-# MPI.Init()
-# atexit(MPI.Finalize)  
-
-# arch = Distributed(GPU(); partition = Partition(y = DistributedComputations.Equal()), synchronized_communication=true)
 
 Nx, Ny, Nz = 10,10,10
 depth = 6000meters
 z_faces = (0,depth)
 
-# grid = LatitudeLongitudeGrid(arch;
-#                              size = (Nx, Ny, Nz),
-#                              halo = (7, 7, 7),
-#                              z = z_faces,
-#                              latitude  = (-75, 75),
-#                              longitude = (0, 360))
-
-grid = RectilinearGrid(arch;
-                       size = (Nx, Ny, Nz),
-                       halo = (1, 2, 1),              # <= This must be ≤ size
-                       x = (0, 1), y = (0, 1), z = (0, 1),
-                       topology = (Periodic, Periodic, Bounded))
+grid = LatitudeLongitudeGrid(arch;
+                             size = (Nx, Ny, Nz),
+                             halo = (7, 7, 7),
+                             z = z_faces,
+                             latitude  = (-75, 75),
+                             longitude = (0, 360))
                          
 free_surface = SplitExplicitFreeSurface(grid; substeps=1)
 @info "Defining Ocean"
 
-# @time ocean = ocean_simulation(grid;
-#                          free_surface)
-@time ocean = HydrostaticFreeSurfaceModel(; grid,
+@time ocean = ocean_simulation(grid;
                          free_surface)
 
-output_path = expanduser("/g/data/v46/txs156/ocean-ensembles/outputs/")
+@info "Defining Atmosphere"
 
-# We force the simulation with an JRA55-do atmospheric reanalysis.
-# @info "Defining Atmosphere"
+radiation  = Radiation(arch)
+atmosphere = JRA55PrescribedAtmosphere(arch; backend=JRA55NetCDFBackend(20))
 
-# radiation  = Radiation(arch)
-# atmosphere = JRA55PrescribedAtmosphere(arch; backend=JRA55NetCDFBackend(20))
+@info "Defining coupled model"
 
-# @info "Defining coupled model"
-
-# coupled_model = OceanSeaIceModel(ocean; atmosphere, radiation)
-# simulation = Simulation(coupled_model; Δt=5minutes, stop_time=20days)
-simulation = Simulation(ocean; Δt=5minutes, stop_time=20days)
+coupled_model = OceanSeaIceModel(ocean; atmosphere, radiation)
+simulation = Simulation(coupled_model; Δt=5minutes, stop_time=20days)
 
 output_path = expanduser("/g/data/v46/txs156/ocean-ensembles/outputs/")
 
 time = 864000.0
 iteration = 2880
 
-ocean.clock.iteration = iteration
-ocean.clock.time = time
+@show simulation.model.ocean.model.clock.iteration = iteration
+@show simulation.model.ocean.model.clock.time = time
+@show simulation.model.atmosphere.clock.iteration = iteration
+@show simulation.model.atmosphere.clock.time = time
+@show simulation.model.clock.iteration = iteration
+@show simulation.model.clock.time = 0.0
 
-# simulation.model.ocean.model.clock.iteration = iteration
-# simulation.model.ocean.model.clock.time = time
-# simulation.model.atmosphere.clock.iteration = iteration
-# simulation.model.atmosphere.clock.time = time
-# simulation.model.clock.iteration = iteration
-# simulation.model.clock.time = time
+@info "Defining messenger"
+
+wall_time = Ref(time_ns())
+
+callback_interval = IterationInterval(20)
+
+function progress(sim)
+    T, S, e = sim.model.ocean.model.tracers
+
+    Trange = (maximum((T)), minimum((T)))
+    Srange = (maximum((S)), minimum((S)))
+    erange = (maximum((e)), minimum((e)))
+        
+    step_time = 1e-9 * (time_ns() - wall_time[])
+
+    msg1 = @sprintf("time: %s, iteration: %d, Δt: %s, ", prettytime(sim), Oceananigans.iteration(sim), prettytime(sim.Δt))
+    # msg2 = @sprintf("max|u|: (%.2e, %.2e, %.2e) m s⁻¹, ", umax...)
+    msg3 = @sprintf("extrema(T): (%.2f, %.2f) ᵒC, ", Trange...)
+    msg4 = @sprintf("extrema(S): (%.2f, %.2f) g/kg, ", Srange...)
+    msg5 = @sprintf("extrema(e): (%.2f, %.2f) J, ", erange...)
+    msg6 = @sprintf("wall time: %s \n", prettytime(step_time))
+
+    @info msg1 * msg3 * msg4 * msg5 * msg6
+
+    wall_time[] = time_ns()
+
+    return nothing
+end
+
+add_callback!(simulation, progress, callback_interval)
 
 # for field in simulation.model.ocean.model.timestepper.G⁻
 #     fill!(field, 0)
